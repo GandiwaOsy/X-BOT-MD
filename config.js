feec4a5c22b58bed47c26f40789bf502d9772b28
GandiwaOsy <osiasmerlin@gmail.com>
2024-07-29 14:59:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ if (fs.existsSync('config.env')) {
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
 const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:3d918c817a2cb478b0c2c2ca8469f68c";
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:201c729c70bcfdf75f31c51e5c61b8b5";
 const SUDO = process.env.SUDO || '917012984396';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
